55e4d990312090e80383065aeef5b2d27169f0a0
caiobasilio <caiobasilio@Caios-MacBook-Pro.local>
2024-10-08 16:50:06 -0300

    Primeiro commit spring-web-swagger.

---
+++ src/main/java/dio/springboot/spring_web_swagger/SpringWebSwaggerApplication.java
@@ -0,0 +1,13 @@
+package dio.springboot.spring_web_swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSwaggerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/dio/springboot/spring_web_swagger/repository/UsuarioRepository.java
@@ -0,0 +1,41 @@
+package dio.springboot.spring_web_swagger.repository;
+
+import dio.springboot.spring_web_swagger.handler.BusinessException;
+import dio.springboot.spring_web_swagger.model.Usuario;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+    public void save(Usuario usuario){
+        if (usuario.getLogin()==null)
+            throw new BusinessException("O campo login é obrigatorio.");
+        if (usuario.getPassword()==null)
+            throw new BusinessException("O campo password é obrigatorio.");
+
+        System.out.println("SAVE - Recebendo o usuário na camada de repositório");
+        System.out.println(usuario);
+    }
+    public void update(Usuario usuario){
+        System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
+        System.out.println(usuario);
+    }
+    public void remove(Integer id){
+        System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
+        System.out.println(id);
+    }
+    public List<Usuario> listAll(){
+        System.out.println("LIST - Listando os usários do sistema");
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("gleyson","password"));
+        usuarios.add(new Usuario("frank","masterpass"));
+        return usuarios;
+    }
+    public Usuario finById(Integer id){
+        System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario("gleyson","password");
+    }
+
+}
